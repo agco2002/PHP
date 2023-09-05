7b55d04c10fd51bb51d9746768c17cbb2ce31689
agco2002 <agco7331@gmail.com>
2023-09-05 15:53:53 -0500

    Nuevo codigo

---
+++ CRUD relacional/Conexion.php
@@ -0,0 +1,14 @@
+<?php
+// Conexión a la base de datos
+// Establecer la conexión con la base de datos
+$servername = "127.0.0.1:3308";
+$username = "root";
+$password = "";
+$dbname = "ecommerce";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Verificar conexión
+if ($conn->connect_error) {
+	die("Conexión fallida: " . $conn->connect_error);
+}
